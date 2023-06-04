069d759c274b6d9210fe913c286a6be889f14b93
yongruifang <1102649231@qq.com>
2023-06-04 23:36:23 +0800

    feat: 封装goodsItem

--- src/views/home/components/homeProduct.vue
+++ src/views/home/components/homeProduct.vue
@@ -1,7 +1,8 @@
 <script setup>
 import homePanel from './homePanel.vue'
 import { getGoodsAPI } from '@/apis/home'
-import { ref,onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
+import goodsItem from './goodsItem.vue'
 const goodsProduct = ref([])
 const getGoods = async () => {
     const { result } = await getGoodsAPI()
@@ -23,12 +24,7 @@ onMounted(() => getGoods())
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy ="good.picture" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-                </RouterLink>
+                <goodsItem :goods="good" />
               </li>
             </ul>
           </div>
@@ -121,42 +117,6 @@ onMounted(() => getGoods())
                 &:nth-child(4n) {
                     margin-right: 0;
                 }
-            }
-        }
-
-        .goods-item {
-            display: block;
-            width: 220px;
-            padding: 20px 30px;
-            text-align: center;
-            transition: all .5s;
-
-            &:hover {
-                transform: translate3d(0, -3px, 0);
-                box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-            }
-
-            img {
-                width: 160px;
-                height: 160px;
-            }
-
-            p {
-                padding-top: 10px;
-            }
-
-            .name {
-                font-size: 16px;
-            }
-
-            .desc {
-                color: #999;
-                height: 29px;
-            }
-
-            .price {
-                color: $priceColor;
-                font-size: 20px;
             }
         }
     }
