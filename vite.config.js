9ae82004ff287723960daf643c3174a8b7ce1096
yongruifang <1102649231@qq.com>
2023-06-04 11:56:02 +0800

    feat: elementPlus自动按需引入

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+// element-plus按需引入
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),    
+  ],
   resolve: {
     // 实际的路径转换工作是由 Node.js 的 fileURLToPath 函数完成的
     alias: {
