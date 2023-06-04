2659d66ce54456ef2abfb559ed8e81442fe5f84d
yongruifang <1102649231@qq.com>
2023-06-04 14:27:56 +0800

    feat: 一级路由

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,6 @@
 </script>
 
 <template>
-<el-button type="primary">Primary</el-button>
+<!-- 一级路由出口组件 -->
+<RouterView />
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import login from '@/views/login/index.vue'
+import layout from '@/views/layout/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      component: layout
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      path: '/login',
+      component: login
     }
   ]
 })
-
 export default router
