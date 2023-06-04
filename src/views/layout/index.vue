70ffed1773ef87fb7af6b235d96b12ba283a8ed8
yongruifang <1102649231@qq.com>
2023-06-04 17:59:25 +0800

    ready: 准备吸顶导航组件

--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,9 +2,11 @@
 import layoutNav from './components/layoutNav.vue'
 import layoutHeader from './components/layoutHeader.vue'
 import layoutFooter from './components/layoutFooter.vue'
+import layoutSticky from './components/layoutSticky.vue'
 </script>
 
 <template>
+    <layoutSticky />
     <layoutNav />
     <layoutHeader />
     <RouterView />
