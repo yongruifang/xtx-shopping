dbf694d412b47c9fdf41a526609949c19b32c0ea
yongruifang <1102649231@qq.com>
2023-06-04 23:49:05 +0800

    feat: 一级路由-资源id路径

--- src/router/index.js
+++ src/router/index.js
@@ -15,7 +15,7 @@ const router = createRouter({
           component: home
         },
         {
-          path: 'category',
+          path: 'category/:id',
           component: category
         }
       ]
