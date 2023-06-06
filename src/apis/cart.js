0cb593f87759b9a32c513c628fe01caa2254495f
yongruifang <1102649231@qq.com>
2023-06-06 23:26:05 +0800

    feat: 接口-合并本地购物车到服务端

--- src/apis/cart.js
+++ src/apis/cart.js
@@ -27,3 +27,11 @@ export const findNewCartListAPI = () => {
         url: '/member/cart',
     })
 }
+// 合并购物车
+export const mergeCartAPI = (data) => {
+    return request({
+        url: '/member/cart/merge',
+        method: 'POST',
+        data
+    })
+}
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,7 +1,8 @@
-import { ref, computed } from 'vue'
+import { ref} from 'vue'
 import { defineStore } from 'pinia'
 import { loginAPI } from '@/apis/user'
 import { useCartStore } from './cartStore'
+import { mergeCartAPI  } from '@/apis/cart'
 
 export const useUserStore = defineStore('user', () => {
     const cartStore = useCartStore()
@@ -11,6 +12,15 @@ export const useUserStore = defineStore('user', () => {
     const getUserInfo = async ({account, password}) => {
         const res = await loginAPI({account, password})
         userInfo.value = res.result
+        // 执行合并购物车的操作
+        await mergeCartAPI(cartStore.cartList.map(item => {
+            return {
+                skuId: item.skuId,
+                selected: item.selected,
+                count: item.count,
+            }
+        }))
+        cartStore.updateNewList()
     }
     // 退出时清除用户信息
     const clearUserInfo = () => {
