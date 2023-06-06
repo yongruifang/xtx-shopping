5e2eff47ac5a467031943412b65e3c68dbeb6261
yongruifang <1102649231@qq.com>
2023-06-06 18:20:09 +0800

    feat: 请求拦截器携带token

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,6 +2,7 @@
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
+import {useUserStore} from '@/stores/user'
 
 const httpInstance = axios.create({
     baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
@@ -11,6 +12,12 @@ const httpInstance = axios.create({
 // axios请求拦截器
 
 httpInstance.interceptors.request.use(config => {
+    //1. 从pinia 获取token数据
+    const userStore = useUserStore()
+    const token = userStore.userInfo.token
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, e => Promise.reject(e))
   
