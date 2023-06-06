179e804d60ca5f646d8119868dbbc348bab8940b
yongruifang <1102649231@qq.com>
2023-06-06 11:03:50 +0800

    feat: 导航栏激活状态显示

--- src/views/layout/components/layoutHeader.vue
+++ src/views/layout/components/layoutHeader.vue
@@ -13,7 +13,7 @@ const categoryStore = useCategoryStore()
                     <RouterLink to="/">首页</RouterLink>
                 </li>
                 <li class="home" v-for="item in categoryStore.categoryList" :key="item.id">
-                    <RouterLink :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
+                    <RouterLink active-class='active' :to="`/category/${item.id}`">{{ item.name }}</RouterLink>
                 </li>
             </ul>
             <div class="search" flex>
@@ -73,8 +73,8 @@ const categoryStore = useCategoryStore()
             }
 
             .active {
-                color: $xtxColor;
-                border-bottom: 1px solid $xtxColor;
+                color:  $xtxColor;
+                border-bottom:  1px solid $xtxColor;
             }
         }
     }
