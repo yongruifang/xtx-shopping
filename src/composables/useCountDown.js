0203e58fda17184e26826f2aa4ced4ae16e79b14
yongruifang <1102649231@qq.com>
2023-06-07 15:43:25 +0800

    optimize: 组件销毁时清除定时器

--- src/composables/useCountDown.js
+++ src/composables/useCountDown.js
@@ -1,9 +1,10 @@
-import { ref, computed } from 'vue'
+import { ref, computed, onUnmounted } from 'vue'
 import dayjs from 'dayjs'
 
 // 封装倒计时的逻辑函数
 export const useCountDown = () => {
     //1. 响应式的数据
+    let timer = null
     const time = ref(0)
     // 格式化时间 xx分xx秒
     const formatTime = computed(() => {
@@ -14,10 +15,14 @@ export const useCountDown = () => {
         //开始倒计时的逻辑
         // 核心： 每隔一秒钟，减1
         time.value = currentTime
-        setInterval(() => {
+        timer = setInterval(() => {
             time.value--
         },1000)
     }
+    // 组件销毁时清除定时器
+    onUnmounted(() => {
+        timer && clearInterval(timer)
+    })
     return {
         formatTime,
         start
